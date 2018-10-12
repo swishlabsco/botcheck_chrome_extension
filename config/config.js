dd7ed251e32e8125adbeb20e68e8e0a53eea781f
marcospgp <marcospereiradev@gmail.com>
2018-10-12 14:58:16 +0100

    Move config to config/config.js

---
+++ config/config.js
@@ -0,0 +1,5 @@
+const botcheckConfig = {
+  // API for production: 'https://botcheck2-dot-surfsafe-rbl.appspot.com',
+  // API for development: 'https://botcheckdummy-dot-surfsafe-rbl.appspot.com'
+  apiRoot: https://botcheckdummy-dot-surfsafe-rbl.appspot.com
+};
