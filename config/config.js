f44cd9966cd5adb08935a5ab2fb94325a97b0c68
marcospgp <marcospereiradev@gmail.com>
2018-10-12 20:15:30 +0100

    Use production endpoint
    Development endpoint was returning an error?

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 const botcheckConfig = {
   // API for production: 'https://botcheck2-dot-surfsafe-rbl.appspot.com',
   // API for development: 'https://botcheckdummy-dot-surfsafe-rbl.appspot.com'
-  apiRoot: 'https://botcheckdummy-dot-surfsafe-rbl.appspot.com'
+  // When changing this value, make sure to also change it in manifest.json's permissions
+  apiRoot: 'https://botcheck2-dot-surfsafe-rbl.appspot.com'
 };
