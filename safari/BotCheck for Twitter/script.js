a9d7663b1c7b977e9370e1a10195ddb1068de827
Adam LeVasseur <adam.j.lev@gmail.com>
2018-10-17 16:25:04 +0000

    Initial safari extension scaffolding

---
+++ safari/BotCheck for Twitter/script.js
@@ -0,0 +1,19 @@
+
+const log = function(...args) {
+    console.log('BotCheck', ...args);
+}
+
+// Dont run inside iframes
+if (window.top === window) {
+    log("injection script on", document.location.href);
+    
+    document.addEventListener("DOMContentLoaded", function(event) {
+      safari.extension.dispatchMessage("Hello World!");
+    });
+}
+else {
+    log('skipped because not top frame')
+}
+
+
+
---
+++ safari/BotCheck/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BotCheck
+//
+//  Created by ajl on 10/17/18.
+//  Copyright © 2018 SwishLabs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // TODO show if extension is enabled or not
+        // https://developer.apple.com/documentation/safariservices/sfsafariextensionmanager
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
