9f217c0e4717332879974a349611a1096af07d17
Adam LeVasseur <adam.j.lev@gmail.com>
2018-10-22 18:40:22 +0000

    [safari] Added Swish team code signing and set deployment target to OSX 10.12

--- safari/BotCheck for Twitter/script.js
+++ safari/BotCheck for Twitter/script.js
@@ -2,16 +2,12 @@
 let runID = Math.random();
 
 const log = function(...args) {
-    console.log('BotCheck', runID, ...args);
+    console.log('(botcheck - safari)', runID, ...args);
 }
 
 // Dont run inside iframes
 if (window.top === window) {
     log("injection script on", document.location.href);
-    
-    document.addEventListener("DOMContentLoaded", function(event) {
-      safari.extension.dispatchMessage("Hello World!");
-    });
 }
 else {
     log('skipped because not top frame')
